ad5416e20f5c54b03ff138984306b5b78390b0d3
Foysal23546 <174034854+Foysal23546@users.noreply.github.com>
2026-05-08 19:54:56 +0600

    Add files via upload

---
+++ 630A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==0)
+        cout<<1<<endl;
+    else if(n==1)
+        cout<<5<<endl;
+    else
+        cout<<25<<endl;
+    return 0;
+}
